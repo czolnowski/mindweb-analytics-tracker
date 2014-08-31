e94fc6697e2325c2d7b3b8b20a42303e7ed4dcd1
Marcin Czołnowski <marcin@czolnowski.net>
2014-08-31 21:45:35 +0200

    Tracker methods.

--- src/tracker.js
+++ src/tracker.js
@@ -6,6 +6,18 @@ var logger = require('mindweb-logger'),
 module.exports.init = function ()
 {
     logger.success('Tracker initialized');
+
+    for (var moduleIndex = 0; moduleIndex < modules.length; ++moduleIndex) {
+        if (modules.hasOwnProperty(moduleIndex)) {
+            var module = modules[moduleIndex];
+
+            if (typeof module.module.init === 'function') {
+                logger.log('Initialize %s module.', module.name);
+
+                module.module.init();
+            }
+        }
+    }
 };
 
 module.exports.register = function (name, module)
@@ -20,16 +32,80 @@ module.exports.register = function (name, module)
     logger.success('Module %s registered', name);
 };
 
-module.exports.run = function ()
+module.exports.run = function (domain, port)
 {
-    var deferred = promise.defer();
+    var deferreds = {
+            connection: promise.defer(),
+            recognizeVisitor: promise.defer(),
+            newVisit: promise.defer(),
+            knownVisit: promise.defer(),
+            registerAction: promise.defer(),
+            end: promise.defer()
+        },
+        deferredsKeys = Object.keys(deferreds);
+
+    port = port | 8080;
+    domain = domain | 'localhost';
+
+    logger.success('Start register deferreds. Modules number: %d.', modules.length);
+
+    for (var moduleIndex = 0; moduleIndex < modules.length; ++moduleIndex) {
+        if (modules.hasOwnProperty(moduleIndex)) {
+            var module = modules[moduleIndex];
+
+            logger.log('--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ');
+            logger.log('Register %s module.', module.name);
+
+            deferredsKeys.forEach(
+                function (key)
+                {
+                    var methodName = 'on' + key.charAt(0).toUpperCase() + key.slice(1);
+
+                    if (typeof module.module[methodName] === 'function') {
+                        logger.log('Register for %s callback %s.', module.name, key);
+
+                        module[methodName](deferreds[key]);
+                    }
+                }
+            );
+        }
+    }
+
+    logger.success('Start server.');
 
     http.createServer(
-        function ()
+        function (request, response)
         {
-            deferred.resolve();
-        }
-    );
+            var startTime = new Date().getTime(),
+                endTime;
+            logger.success('New connection. [%d]', startTime);
+            deferreds.connection.resolve(request, response);
 
-    return deferred.promise;
+            var visitor = null;
+
+            deferreds.recognizeVisitor(visitor, request, response);
+            logger.success('Recognize visitor.');
+
+            if (visitor === null) {
+                deferreds.newVisit(visitor, request, response);
+                logger.success('New visit.');
+            } else {
+                deferreds.knownVisit(visitor, request, response);
+                logger.success('Visit known.');
+            }
+
+            deferreds.registerAction(visitor, request, response);
+            logger.success('Register action.');
+
+            endTime = new Date().getTime();
+
+            logger.success('Action end. [%d]', endTime);
+
+            logger.success('Request handled in %d ms seconds.', endTime - startTime);
+        }
+    ).listen(port | 8080, domain | 'localhost');
+
+    logger.success('Server is running!');
+
+    return deferreds.connection.promise;
 };
